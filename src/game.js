18e672e2aa95df1fd6ae986b6225b48750e31857
WooLyung Lee <vlfkdydtk@naver.com>
2018-09-28 11:37:24 +0900

    add scene, player class, enemy class, and simplify source

--- src/game.js
+++ src/game.js
@@ -1,138 +1,178 @@
-var game = new Phaser.Game(innerWidth, innerHeight, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+// variables
+var foodChain = [];
+var enemies = [];
 
-// Player
-var player =
+// Scene
+var inGame = 
 {
-    body:undefined,
-    tail:[]
-};
-
-player.addTail = function()
-{
-    // Create Tail
-    if (player.tail.length == 0)
+    preload : function()
     {
-        player.tail.push(game.add.sprite(player.body.body.position.x, player.body.body.position.y, 'tail'));
-    }
-    else
+        game.load.image('body', 'assets/sprites/body.png');
+        game.load.image('tail', 'assets/sprites/tail.png');
+        game.load.image('background', 'assets/sprites/background.jpg');
+    },
+    create : function()
     {
-        player.tail.push(game.add.sprite(player.tail[player.tail.length - 1].body.position.x, player.tail[player.tail.length - 1].body.position.y, 'tail'));
-    }
-
-    player.tail[player.tail.length - 1].anchor.setTo(0.5, 0.5);
-    game.physics.arcade.enable(player.tail[player.tail.length - 1]);
-    player.tail[player.tail.length - 1].tint = game.rnd.integerInRange(0, Math.pow(16, 6) - 1);
-
-    // Set Tails's Scale and Layer
-    for (let i = player.tail.length - 1; i >= 0; i--)
-    {
-        game.world.bringToTop(player.tail[i]);
-        player.tail[i].scale.setTo(0.2 + (player.tail.length - i - 1) * 0.8 / (player.tail.length), 0.2 + (player.tail.length - i - 1) * 0.8 / (player.tail.length));
-    }
-    game.world.bringToTop(player.body);
-}
-
-player.update = function()
-{
-    player.move();
-}
-
-player.move = function()
-{
-    let defaultSpeed = 300;
-    let speed = defaultSpeed;
-    let interval = 0.5;
-    if (game.input.activePointer.leftButton.isDown)
-    {
-        speed = 600;
-        interval = 1;
-    }
-
-    // Body Move
-    if (Util.distance(player.body.body.position.x + player.body.width / 2, player.body.body.position.y + player.body.height / 2, game.input.activePointer.x + game.camera.position.x, game.input.activePointer.y + game.camera.position.y) <= player.body.width / 2)
-    {
-        player.body.body.velocity.setTo(0, 0);
-    }
-    else
-    {
-        player.body.body.velocity.x = speed * Math.cos(game.physics.arcade.angleBetween(player.body, {x:game.input.activePointer.position.x + game.camera.position.x, y:game.input.activePointer.position.y + game.camera.position.y}));
-        player.body.body.velocity.y = speed * Math.sin(game.physics.arcade.angleBetween(player.body, {x:game.input.activePointer.position.x + game.camera.position.x, y:game.input.activePointer.position.y + game.camera.position.y}));
-    }
-    player.body.rotation = game.physics.arcade.angleBetween(player.body, {x:game.input.activePointer.position.x + game.camera.position.x, y:game.input.activePointer.position.y + game.camera.position.y});
-
-    // Tail Move
-    for (let i = 0; i < player.tail.length; i++)
-    {
-        if (i == 0)
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.stage.backgroundColor = '#99ffcc';
+    
+        game.add.tileSprite(0, 0, 5000, 5000, 'background'); 
+        game.world.setBounds(0, 0, 5000, 5000);
+    
+        this.player = new Player();
+    
+        for (let i = 0; i < 10; i++)
         {
-            let distance = Util.distance(player.tail[i].position.x - player.tail[i].width/2, player.tail[i].position.y - player.tail[i].height/2, player.body.position.x - player.body.width/2, player.body.position.y - player.body.height/2);
-            if (distance <= (player.tail[i].height + player.tail[i].height) / 2 * interval)
-            {
-                player.tail[i].body.velocity.setTo(0, 0);
-            }
-            else
-            {
-                player.tail[i].body.velocity.x = distance / player.tail[i].width * defaultSpeed * Math.cos(game.physics.arcade.angleBetween({x:(player.tail[i].position.x - player.tail[i].width/2), y:(player.tail[i].position.y - player.tail[i].height/2)}, {x:(player.body.position.x - player.body.width/2), y:(player.body.position.y - player.body.height/2)}));
-                player.tail[i].body.velocity.y = distance / player.tail[i].width * defaultSpeed * Math.sin(game.physics.arcade.angleBetween({x:(player.tail[i].position.x - player.tail[i].width/2), y:(player.tail[i].position.y - player.tail[i].height/2)}, {x:(player.body.position.x - player.body.width/2), y:(player.body.position.y - player.body.height/2)}));
-            }
-            player.tail[i].rotation = game.physics.arcade.angleBetween(player.tail[i], player.body);
+            this.player.addTail();
+        }
+    },
+    update : function()
+    {
+        this.player.update();
+        if (game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).justDown)
+        {
+            this.player.addTail();
+        }
+    },
+    render : function()
+    {
+        game.debug.cameraInfo(game.camera, 32, 32);
+    }
+}
+
+// Classes
+class Player
+{
+    constructor()
+    {
+        this.tail = [];
+        this.body = game.add.sprite(game.world.centerX, game.world.centerY, 'body');
+        this.body.anchor.setTo(0.5, 0.5);
+        game.physics.arcade.enable(this.body);
+        game.camera.follow(this.body);
+        this.body.tint = game.rnd.integerInRange(0, Math.pow(16, 6) - 1);
+    }
+
+    addTail()
+    {
+        // Create Tail
+        if (this.tail.length == 0)
+        {
+            this.tail.push(game.add.sprite(this.body.position.x, this.body.position.y, 'tail'));
         }
         else
         {
-            let distance = Util.distance(player.tail[i].position.x, player.tail[i].position.y, player.tail[i - 1].position.x, player.tail[i - 1].position.y);
-            if (distance <= (player.tail[i].height + player.tail[i - 1].height) / 2 * interval)
+            this.tail.push(game.add.sprite(this.tail[this.tail.length - 1].body.position.x, this.tail[this.tail.length - 1].body.position.y, 'tail'));
+        }
+    
+        let newTail = this.tail[this.tail.length - 1];
+
+        newTail.anchor.setTo(0.5, 0.5);
+        game.physics.arcade.enable(newTail);
+        newTail.tint = game.rnd.integerInRange(0, Math.pow(16, 6) - 1);
+    
+        // Set Tails's Scale and Layer
+        for (let i = this.tail.length - 1; i >= 0; i--)
+        {
+            game.world.bringToTop(this.tail[i]);
+            this.tail[i].scale.setTo(0.2 + (this.tail.length - i - 1) * 0.8 / (this.tail.length), 0.2 + (this.tail.length - i - 1) * 0.8 / (this.tail.length));
+        }
+        game.world.bringToTop(this.body);
+    }
+    
+    update()
+    {
+        this.move();
+    }
+    
+    move()
+    {
+        let defaultSpeed = 300;
+        let speed = defaultSpeed;
+        if (game.input.activePointer.leftButton.isDown)
+        {
+            speed = 600;
+        }
+    
+        // Body Move
+        let x1 = this.body.position.x + this.body.width / 2;
+        let y1 = this.body.position.y + this.body.height / 2;
+        let x2 = game.input.activePointer.x + game.camera.position.x;
+        let y2 = game.input.activePointer.y + game.camera.position.y;
+        let angle = game.physics.arcade.angleBetween(this.body, {x:game.input.activePointer.position.x + game.camera.position.x, y:game.input.activePointer.position.y + game.camera.position.y});
+        
+        if (Util.distance(x1, y1, x2, y2) <= this.body.width / 2)
+        {
+            this.body.body.velocity.setTo(0, 0);
+        }
+        else
+        {
+            
+            this.body.body.velocity.x = speed * Math.cos(angle);
+            this.body.body.velocity.y = speed * Math.sin(angle);
+        }
+        this.body.rotation = angle;
+    
+        // Tail Move
+        for (let i = 0; i < this.tail.length; i++)
+        {
+            if (i == 0)
             {
-                player.tail[i].body.velocity.setTo(0, 0);
+                let distance = Util.distance(this.tail[i].position.x - this.tail[i].width/2, this.tail[i].position.y - this.tail[i].height/2, this.body.position.x - this.body.width/2, this.body.position.y - this.body.height/2);
+                if (distance <= (this.tail[i].height + this.tail[i].height) / 4)
+                {
+                    this.tail[i].body.velocity.setTo(0, 0);
+                }
+                else
+                {
+                    let angle = game.physics.arcade.angleBetween({x:(this.tail[i].position.x - this.tail[i].width/2), y:(this.tail[i].position.y - this.tail[i].height/2)}, {x:(this.body.position.x - this.body.width/2), y:(this.body.position.y - this.body.height/2)});
+                    
+                    this.tail[i].body.velocity.x = distance / this.tail[i].width * defaultSpeed * Math.cos(angle);
+                    this.tail[i].body.velocity.y = distance / this.tail[i].width * defaultSpeed * Math.sin(angle);
+                }
+                this.tail[i].rotation = game.physics.arcade.angleBetween(this.tail[i], this.body);
             }
             else
             {
-                player.tail[i].body.velocity.x = distance / player.tail[i].width * defaultSpeed * Math.cos(game.physics.arcade.angleBetween(player.tail[i], player.tail[i-1]));
-                player.tail[i].body.velocity.y = distance / player.tail[i].width * defaultSpeed * Math.sin(game.physics.arcade.angleBetween(player.tail[i], player.tail[i-1]));
+                let distance = Util.distance(this.tail[i].position.x, this.tail[i].position.y, this.tail[i - 1].position.x, this.tail[i - 1].position.y);
+                if (distance <= (this.tail[i].height + this.tail[i - 1].height) / 4)
+                {
+                    this.tail[i].body.velocity.setTo(0, 0);
+                }
+                else
+                {
+                    let angle = game.physics.arcade.angleBetween(this.tail[i], this.tail[i-1]);
+                    this.tail[i].body.velocity.x = distance / this.tail[i].width * defaultSpeed * Math.cos(angle);
+                    this.tail[i].body.velocity.y = distance / this.tail[i].width * defaultSpeed * Math.sin(angle);
+                }
+                this.tail[i].rotation = game.physics.arcade.angleBetween(this.tail[i], this.tail[i-1]);
             }
-            player.tail[i].rotation = game.physics.arcade.angleBetween(player.tail[i], player.tail[i-1]);
         }
+    }
+}
+
+class Enemy
+{
+    constructor()
+    {
+        this.body = undefined;
+        this.tail = [];
+    }
+
+    static getDataFromServer()
+    {
+
+    }
+
+    getDataEach()
+    {
+
     }
 }
 
 // Game
-function preload()
-{
-    game.load.image('body', 'sprites/body.png');
-    game.load.image('tail', 'sprites/tail.png');
-    game.load.image('background', 'sprites/background.jpg');
-}
+var game = new Phaser.Game(innerWidth, innerHeight, Phaser.CANVAS);
 
-function create()
-{
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.stage.backgroundColor = '#99ffcc';
+game.state.add('inGame', inGame);
 
-    game.add.tileSprite(0, 0, 5000, 5000, 'background'); 
-    game.world.setBounds(0, 0, 5000, 5000);
-
-    player.body = game.add.sprite(game.world.centerX, game.world.centerY, 'body');
-    player.body.anchor.setTo(0.5, 0.5);
-    game.physics.arcade.enable(player.body);
-    game.camera.follow(player.body);
-
-    player.body.tint = game.rnd.integerInRange(0, Math.pow(16, 6) - 1);
-
-    for (let i = 0; i < 10; i++)
-    {
-        player.addTail();
-    }
-}
-
-function update()
-{
-    player.update();
-    if (game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).justDown)
-    {
-        player.addTail();
-    }
-}
-
-function render()
-{
-    game.debug.cameraInfo(game.camera, 32, 32);
-}
+game.state.start('inGame');
