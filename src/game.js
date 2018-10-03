ee51e2dc42a2444eabbbd5d5ae0195c595e2d8df
WooLyung Lee <vlfkdydtk@naver.com>
2018-10-03 14:54:07 +0900

    add game end

--- src/game.js
+++ src/game.js
@@ -5,7 +5,35 @@ let roomid = -1;
 let status = 0;
 let player = {};
 
+let screenHeight = innerHeight;
+let screenWidth = innerWidth;
+
 // Scene
+let main = 
+{
+    preload : function()
+    {
+        game.load.image('background', 'src/assets/sprites/background.jpg');
+        game.load.image('logo', 'src/assets/sprites/logo.png');
+    },
+
+    create : function()
+    {
+        game.add.tileSprite(0, 0, 2000, 2000, 'background');
+        game.add.sprite(screenWidth/2, screenHeight/2, 'logo').anchor.setTo(0.5, 0.5);
+    },
+
+    update : function()
+    {
+        if (game.input.activePointer.leftButton.isDown)
+            game.state.start('waiting');
+    },
+
+    render : function()
+    {
+    }
+}
+
 let waiting = 
 {
     preload : function()
@@ -15,11 +43,7 @@ let waiting =
 
     create : function()
     {
-        game.add.tileSprite(0, 0, 5000, 5000, 'background'); 
-        game.world.setBounds(0, 0, 5000, 5000);
-        var style = { font: "bold 50px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
-        this.waitingText = game.add.text(100, 100, "3 / 4", style)
-        this.waitingText.setShadow(3, 3, 'rgba(0,0,0,1)', 2);
+        game.add.tileSprite(0, 0, 2000, 2000, 'background');
     },
 
     update : function()
@@ -48,11 +72,9 @@ let inGame =
 
     create : function()
     {
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-        game.stage.backgroundColor = '#99ffcc';
-    
-        game.add.tileSprite(0, 0, 5000, 5000, 'background'); 
-        game.world.setBounds(0, 0, 5000, 5000);
+        game.physics.startSystem(Phaser.Physics.ARCADE);    
+        game.add.tileSprite(0, 0, 2000, 2000, 'background'); 
+        game.world.setBounds(0, 0, 1000, 1000);
     
         player = new Player();
         player.body.tint = 0x2EFE2E;
@@ -63,13 +85,22 @@ let inGame =
     update : function()
     {
         player.update();
-        if (game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).justDown)
-            player.addTail();
+
+        this.backWaiting();
     },
 
     render : function()
     {
         game.debug.text(1 / game.time.physicsElapsed, 32, 32);
+    },
+
+    backWaiting : function()
+    {
+        if (game.input.activePointer.leftButton.isDown && roomid === -2)
+        {
+            roomid = -1;
+            game.state.start('waiting');
+        }
     }
 }
 
@@ -112,7 +143,8 @@ class Player
     {
         this.move();
         this.tailCollision();
-        this.emitDataToServer();
+        if (status === 0 || foodChain.findIndex(chain => (chain.hunter === socket.id)) !== -1 )
+            this.emitDataToServer();
     }
     
     move()
@@ -212,24 +244,39 @@ class Player
     
             function isCollide(tail, head, width)
             {
-                return Util.doubleDistance(tail, head) <= Math.pow(width / 2, 2);
+                return Util.doubleDistance(tail, head) <= Math.pow(width / 1.5, 2);
             }
     
             function collision(enemy, collideTail)
             {
-                if (foodChain.find(chain => (chain.hunter == socket.id && chain.target == enemy.id)))
+                if (foodChain.find(chain => (chain.hunter === socket.id && chain.target === enemy.id)))
                 {
                     socket.emit('died', {hunter:socket.id, target:enemy.id});
                 }
-                else if (foodChain.find(chain => (chain.target == socket.id && chain.hunter == enemy.id)))
+                else if (foodChain.find(chain => (chain.target === socket.id && chain.hunter === enemy.id)))
                 {
                 }
-                else
+                else if (foodChain.findIndex(chain => chain.hunter === socket.id) !== -1)
                 {
                     player.bounce.x = 1000 * Math.cos(game.physics.arcade.angleBetween(collideTail.position, player.body.position));
                     player.bounce.y = 1000 * Math.sin(game.physics.arcade.angleBetween(collideTail.position, player.body.position));
                 }
             }
+        }
+    }
+
+    gameEnd(value)
+    {
+        let style = { font: "bold 50px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+        if (value.winner.id === socket.id)
+        {
+            this.waitingText = game.add.text(game.camera.position.x + screenWidth/2, game.camera.position.y + screenHeight/2, "You Win!", style)
+            this.waitingText.setShadow(3, 3, 'rgba(0,0,0,1)', 2);
+        }
+        else
+        {
+            this.waitingText = game.add.text(game.camera.position.x + screenWidth/2, game.camera.position.y + screenHeight/2, "You Lose!", style)
+            this.waitingText.setShadow(3, 3, 'rgba(0,0,0,1)', 2);
         }
     }
 
@@ -279,14 +326,19 @@ class Enemy
         // Save User's roomid
         roomid = enemies.find(enemy => enemy.id == socket.id).roomid;
 
-        //Destroy object that's oneself
-        enemies.splice(enemies.findIndex(enemy => enemy.id == socket.id), 1);
-        
-        // Add object on enemies array
-        enemies.forEach((enemy, index) => {
-            enemiesData.push(new Enemy());
-            enemiesData[index].getDataEach(enemy);
-        });
+        if (roomid !== -2)
+        {
+            //Destroy object that's oneself
+            enemies.splice(enemies.findIndex(enemy => enemy.id == socket.id), 1);
+            
+            // Add object on enemies array
+            enemies.forEach((enemy) => {
+                if (!enemy.isDead)
+                {
+                    enemiesData[enemiesData.push(new Enemy()) - 1].getDataEach(enemy);
+                }
+            });
+        }   
     }
 
     getDataEach(data)
@@ -324,13 +376,23 @@ socket.on("update", function(data)
     Enemy.getDataFromServer(data.users);
 });
 socket.on("died", () => {
-    close();
+    player.body.alpha = 0.5;
+    player.body.tint = 0x999999;
+    player.tail.forEach(tail => {
+        tail.alpha = 0.5;
+        tail.tint = 0x999999;
+    })
 });
+socket.on("gameEnd", (value) => {
+    roomid = -2;
+    player.gameEnd(value);
+})
 
 // Game
-let game = new Phaser.Game(innerWidth, innerHeight, Phaser.CANVAS);
+let game = new Phaser.Game(screenWidth, screenHeight, Phaser.CANVAS);
 
 game.state.add('inGame', inGame);
 game.state.add('waiting', waiting);
+game.state.add('main', main);
 
-game.state.start('waiting');
+game.state.start('main');
